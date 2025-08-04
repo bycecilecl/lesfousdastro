0218fbef366f07728a5d1ae80cac5c16b78a133e
bycecilecl <cecilecl.cyp@gmail.com>
2025-08-04 18:50:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY")
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "jcg)c+og9c47o=cufp-%)o6q@$14#8980_(z23a$+t771sn!@*")
 register_routes(app)
 
 
